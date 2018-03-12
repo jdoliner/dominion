4e36c33d46a4a317aee9ba4be70f29deeb1564bf
Joe Doliner <jdoliner@gmail.com>
2018-03-11 20:54:46 -0700

    Adds start of dominion engine implementation.

---
+++ src/dominion/dominion.go
@@ -0,0 +1,52 @@
+package dominion
+
+type Game struct {
+	Players []*Player
+	Trash   []*Card
+}
+
+func NewGame(players int) *Game {
+	result := &Game{}
+	for i := 0; i < players; i++ {
+		result.Players = append(result.Players, NewPlayer(nil))
+	}
+	return result
+}
+
+type Player struct {
+	Actions int
+	Coin    int
+	Buys    int
+	Deck    []Card
+	Hand    []Card
+	Discard []Card
+	Play    []Card
+}
+
+func NewPlayer(deck []Card) *Player {
+	return &Player{Deck: deck}
+}
+
+type Type int64
+
+const (
+	Victory Type = 1 << iota
+	Treasure
+	Action
+	Curse
+)
+
+type Card struct {
+	Name  string
+	Type  Type
+	Cost  int
+	Score int
+	Play  func(*Game)
+}
+
+type Choice struct {
+	Make        func(*Game)
+	Description string
+}
+
+type Decision []Choice
