f42ccd1d36bac18cc2f3f3344c5ef07915b044f2
Joe Doliner <jdoliner@gmail.com>
2018-05-28 13:44:11 -0700

    Adds first test.

---
+++ src/dominion/dominion_test.go
@@ -0,0 +1,39 @@
+package dominion
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func countCards(cs Cards) map[string]int {
+	result := make(map[string]int)
+	for _, c := range cs {
+		result[c.Name]++
+	}
+	return result
+}
+
+func TestGame(t *testing.T) {
+	g := NewGame(2)
+	assert.Equal(t, 2, len(g.Players))
+	assert.Equal(t, 0, g.active)
+	assert.Equal(t, 0, g.Turn)
+	p1Start, p2Start := countCards(append(g.Players[0].Deck, g.Players[0].Hand...)), countCards(append(g.Players[1].Deck, g.Players[1].Hand...))
+	assert.Equal(t, 3, p1Start["Estate"])
+	assert.Equal(t, 7, p1Start["Copper"])
+	assert.Equal(t, 3, p2Start["Estate"])
+	assert.Equal(t, 7, p2Start["Copper"])
+	assert.Equal(t, 5, len(g.Players[0].Hand))
+	assert.Equal(t, 5, len(g.Players[1].Hand))
+	assert.Equal(t, 5, len(g.Players[0].Deck))
+	assert.Equal(t, 5, len(g.Players[1].Deck))
+
+	g.ActivePlayer().PlayTreasures()
+	hand, play := countCards(g.ActivePlayer().Hand), countCards(g.ActivePlayer().Play)
+	assert.Equal(t, 0, hand["Copper"])
+	assert.Equal(t, 0, play["Estate"])
+	assert.Equal(t, 5, hand["Estate"]+play["Copper"])
+	fmt.Printf("%v", g.ActivePlayer())
+}
